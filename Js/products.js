71f89046329b83ea626547e15e736269f5c9336f
mdoliullah4590-ai <mdoliullah4590@gmail.com>
2026-03-08 23:53:07 +0600

    Create products.js

---
+++ Js/products.js
@@ -0,0 +1,50 @@
+// Product Database
+
+let products = [
+
+{
+id: 1,
+name: "Luxury T-Shirt",
+price: 1200,
+cat: "tshirt",
+img: "https://via.placeholder.com/300",
+details: "Premium cotton luxury T-shirt."
+},
+
+{
+id: 2,
+name: "Luxury Hoodie",
+price: 2200,
+cat: "hoodie",
+img: "https://via.placeholder.com/300",
+details: "Warm stylish hoodie for winter."
+},
+
+{
+id: 3,
+name: "Luxury Jacket",
+price: 3500,
+cat: "jacket",
+img: "https://via.placeholder.com/300",
+details: "Premium winter jacket."
+},
+
+{
+id: 4,
+name: "Streetwear T-Shirt",
+price: 1500,
+cat: "tshirt",
+img: "https://via.placeholder.com/300",
+details: "Modern streetwear style."
+},
+
+{
+id: 5,
+name: "Black Hoodie",
+price: 2400,
+cat: "hoodie",
+img: "https://via.placeholder.com/300",
+details: "Minimal luxury hoodie."
+}
+
+];
